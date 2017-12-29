3264d252aa18d3a204ad3719e97eb3a14e7cd4b3
Jess <j.martin@i2tic.com>
2017-12-29 14:14:51 +0100

    Implementación de las 3 tablas del modelo a falta de las FK.

--- gdocimport/models.py
+++ gdocimport/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
 # Create your models here.
+
+
+class HigherEducationInstitutes(models.Model):
+
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+
+    class Meta:
+        verbose_name = 'HEI'
+        verbose_name_plural = 'HEIS'
+
+    def __str__(self):
+        return self.name
+
+
+class OrganizationUnit(models.Model):
+
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=200)
+    parent_ounit = models.IntegerField()
+    subject = models.ForeignKey()
+
+    class Meta:
+        verbose_name = 'OUNIT'
+        verbose_name_plural = 'OUNITS'
+
+    def __str__(self):
+        return self.name
+
+
+class Subject(models.Model):
+
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=200)
+    area = models.CharField(max_length=200)
+    isced_code = models.IntegerField()
+    credit_scheme = models.IntegerField()
+    credit_level = models.CharField(max_length=200)
+    credit_value = models.IntegerField()
+
+    class Meta:
+        verbose_name = 'LOS'
+        verbose_name_plural = 'SUBJECTS'
+
+    def __str__(self):
+        return self.name
