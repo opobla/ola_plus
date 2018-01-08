4eecf11d13eaeb22347e3743af27328b9cdf7a6a
Andrés Usán <a.usan@i2tic.com>
2018-01-08 14:26:26 +0100

    Importador y exportador de fixtures

---
+++ core/management/commands/fixture_loader.py
@@ -0,0 +1,28 @@
+from django.core.management.base import BaseCommand
+from django.core.management import call_command
+from django.db.utils import IntegrityError
+from glob import glob
+
+
+class Command(BaseCommand):
+    help = 'Fixture loader. Imports models from fixtures.'
+
+    def add_arguments(self, parser):
+        pass
+
+    def get_fixtures(self):
+        return glob("core/fixtures/*.json")
+
+    def handle(self, *args, **options):
+        if options["settings"] == "ola_plus.settings.production":
+            fix = input("Fixtures will be imported in the PRODUCTION environment. Are you sure? [Y/n] ")
+            if fix != "Y":
+                self.stderr.write("Operation cancelled.")
+                return False
+
+        for fixture in self.get_fixtures():
+            try:
+                self.stdout.write("Fixture for: " + fixture)
+                call_command('loaddata', fixture)
+            except IntegrityError as e:
+                self.stdout.write("Fixture: " + fixture + " FAILED", e.args)
